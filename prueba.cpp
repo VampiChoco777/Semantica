92936598874792eeea8329a8b0776867468bad1d
VampiChoco777 <l20140892@queretaro.tecnm.mx>
2022-10-09 23:35:23 -0500

    09 oct 2022

--- prueba.cpp
+++ prueba.cpp
@@ -3,16 +3,45 @@
 #include <stdio.h>
 #include <conio.h>
 float area, radio, pi, resultado;
-int a , d;
-char x;
+int a, d, altura;
+float x;
+char y;int ide ;int j;
 // Este programa calcula el volumen de un cilindro.
 void main(){
-    x = (char) (a);
-    if(8 < 10)
-    {
-        if(1==1){
-            printf("Holi crayoli\n");
-            scanf("%f",&area);
+    //Requerimiento 5.- Levanta una excepcion en el scanf si la captura no es un numero
+    printf("Introduce la altura de la piramide: ");
+    scanf("altura", &altura);
+    //Requerimiento 6.- Ejecutar el for y for anidado
+    
+    if(altura >2)
+
+        for(ide = altura; ide > 0; ide--)
+        {
+        printf(i);
+            for(j = 0; j < altura-ide; j++){
+                if(j!=2){
+                    printf("*");
+                }
+                else{
+                    printf("-");//Requerimiento 4.- evalua nuevamente los else
+                }
+            }
+            printf("\n");
+        }
+    else
+        printf("\nError: la altura debe de ser mayor que 2\n");
+    if(1 != 1){
+        printf("Esto no se debe imprimir");
+        if(2 == 2){
+            printf("Esto tampoco");     //Requerimiento 4.- evalua nuevamente los if respecto al parametro que reciben
         }
     }
+    a = 256;
+    printf("Valor de variable int a antes del casteo: ");
+    printf(a);
+    y = (char)(a);  //Requerimiento 2 y 3, actualiza el dominante y convierte el valor con una funcion
+    printf("\nValor de variable char y despues del casteo de a: ");
+    printf(y);
+    printf("\nA continuacion se intenta asignar un int a un char sin usar casteo: \n");
+    y = a; //Requerimiento 1.- debe marcar error
 }
